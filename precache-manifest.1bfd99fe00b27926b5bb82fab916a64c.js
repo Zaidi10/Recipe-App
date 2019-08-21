be6609d0cb0a779bbeb5ce23a1ac8f818137a703
Zaidi10 <16ume051@lnmiit.ac.in>
2019-08-22 03:49:31 +0530

    Updates

---
+++ precache-manifest.1bfd99fe00b27926b5bb82fab916a64c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e0bf195c4be57c2b20e82d4b1e8db4",
+    "url": "/Recipe-App/index.html"
+  },
+  {
+    "revision": "526655345739f1956359",
+    "url": "/Recipe-App/static/css/main.5bf7cf70.chunk.css"
+  },
+  {
+    "revision": "dabe9d9256da3eb2b393",
+    "url": "/Recipe-App/static/js/2.5e8e749c.chunk.js"
+  },
+  {
+    "revision": "526655345739f1956359",
+    "url": "/Recipe-App/static/js/main.8aecbfcc.chunk.js"
+  },
+  {
+    "revision": "1ca18928a48970ae9c6c",
+    "url": "/Recipe-App/static/js/runtime~main.77a5bd4a.js"
+  }
+]);
